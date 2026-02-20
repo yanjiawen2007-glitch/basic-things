d271b56503f43cbcd53643ef8e3e18d5c6d98033
yanjiawen2007-glitch <yanjiawen2007@gmail.com>
2026-02-20 21:03:49 +0800

    Update init.py

--- app/routers/init.py
+++ app/routers/init.py
@@ -1,1 +1,8 @@
-routers/init.py
+"""
+API routers
+"""
+from app.routers.tasks import router as tasks_router
+from app.routers.ai import router as ai_router
+from app.routers.messages import router as messages_router
+
+__all__ = ["tasks_router", "ai_router", "messages_router"]
